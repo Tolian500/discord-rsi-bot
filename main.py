7f74f3250277208beef9b54022caf3055aaca950
Your Name <you@example.com>
2024-06-22 18:01:32 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas
+import discord
+from discord.ext import commands
+import requests
+import pybit
+# import TA-lib
+import os
+
+
+BOT_TOKEN = os.environ["discord-bot-token"]
+DISCORD_CS = os.environ["discord-CS"]
+DISCORD_APP_ID = os.environ["discord-app-id"]
+
+
+
+
+
+
